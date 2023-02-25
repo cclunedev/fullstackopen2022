b7730eee7757ce59d5054fe44a2089469c82aa88
cclune <cclune@mrfreeze.totalrecall.net>
2023-02-25 10:14:22 -0500

    Finishes Exercises 2.2-2.5

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,7 +1,43 @@
-const Course = ({ course }) => {
-    return (
-      <li>{course.name}</li>
-    )
-  }
-  
-  export default Course
+const Header = (props) => {
+  return(
+    <h1>{props.course.name}</h1>
+  )
+}
+
+const Part = ({name, exercises}) => {
+  return(
+    <p>{name} {exercises}</p>
+  )
+}
+
+const Total = ({parts}) => {
+  const total = parts.reduce((s, p) => {return s + p.exercises}, 0)
+
+  return(
+    <p><b>total of {total} exercises</b></p>
+  )
+}
+
+const Content = ({course}) => {
+  return(
+  <div>
+      {course.parts.map(parts=>
+        <Part key={parts.id} name={parts.name} exercises={parts.exercises} />
+      )}
+  <Total parts={course.parts} />
+  </div>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <div>
+    <Header course={course} />
+    
+    <Content course ={course} />
+    </div>
+    
+  )
+}
+
+export default Course
