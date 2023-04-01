7e2556f81ae8ded1b0dcbd6d6724a44fffa9abf1
cclune <cclunedev@gmail.com>
2023-04-01 14:49:48 -0400

    Completes Exercise 4.2. Restructures blog application into app, model, controllers, utils modules.

--- part4/blog/app.js
+++ part4/blog/app.js
@@ -5,34 +5,25 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const Blog = require('./models/blog.js')
+const blogsRouter = require('./controllers/blogs')
+const logger = require('./utils/logger')
+const middleware = require('./utils/middleware')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl)
+mongoose.set('strictQuery', false)
 
-app.use(cors())
-app.use(express.json())
+logger.info('connecting to', config.MONGODB_URI)
 
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+mongoose.connect(config.MONGODB_URI)
 
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
 
 app.use(cors())
 app.use(express.static('build'))
 app.use(express.json())
+app.use(middleware.requestLogger)
 
+app.use('/api/blogs', blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
 
 module.exports = app
