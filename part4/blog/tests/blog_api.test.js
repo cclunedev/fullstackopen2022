750afc818715e5710d2e50b054cd3e477252a46a
cclune <cclunedev@gmail.com>
2023-05-20 17:08:36 -0400

    Refactors Blogs API tests into groups

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -14,88 +14,94 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('get /api/blogs returns the correct number of blogs', async () => {
-    const response = await api.get('/api/blogs')
-  
-    expect(response.body).toHaveLength(helper.initialBlogs.length)
-})
-
-test('the unique identifier property of the blog posts is named id', async () => {
-    const response = await api.get('/api/blogs')
-    expect(response.body[0].id).toBeDefined()
-})
-
-test('post /api/blogs request creates a new blog post', async () => {
-    const newBlog = {
+describe('GET /api/blogs data validation', () => {
+    test('GET /api/blogs returns the correct number of blogs', async () => {
+        const response = await api.get('/api/blogs')
     
-        title: 'Ripping Dingers',
-        author: 'Connor C',
-        url: 'https://hittingtanks.com',
-        likes: 1000
-    }
+        expect(response.body).toHaveLength(helper.initialBlogs.length)
+    })
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(201)
-        .expect('Content-Type', /application\/json/)
-
-    
-    const response = await api.get('/api/blogs')
-    const contents = response.body.map(r => r.title)
-    expect(contents).toContain(
-        'Ripping Dingers'
-    )
-    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+    test('the unique identifier property of the blog posts is named id', async () => {
+        const response = await api.get('/api/blogs')
+        expect(response.body[0].id).toBeDefined()
+    })
 
 })
 
-test('post /api/blogs will default likes to 0 if not provided', async () => {
-    const newBlog = {
-    
-        title: 'Nobody Likes This',
-        author: 'Connor C',
-        url: 'https://nobodylikesthis.com'
-    }
 
-    const response = await api.post('/api/blogs').send(newBlog)
-    const contents = response.body
-    expect(response.body.likes).toBeDefined()
+describe('POST /api/blogs validation', () => {
+    test('post /api/blogs request creates a new blog post', async () => {
+        const newBlog = {
+        
+            title: 'Ripping Dingers',
+            author: 'Connor C',
+            url: 'https://hittingtanks.com',
+            likes: 1000
+        }
+
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(201)
+            .expect('Content-Type', /application\/json/)
+
+        
+        const response = await api.get('/api/blogs')
+        const contents = response.body.map(r => r.title)
+        expect(contents).toContain(
+            'Ripping Dingers'
+        )
+        expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+
+    })
+
+    test('post /api/blogs will default likes to 0 if not provided', async () => {
+        const newBlog = {
+        
+            title: 'Nobody Likes This',
+            author: 'Connor C',
+            url: 'https://nobodylikesthis.com'
+        }
+
+        const response = await api.post('/api/blogs').send(newBlog)
+        const contents = response.body
+        expect(response.body.likes).toBeDefined()
 
 
 
-})
+    })
 
-test('post /api/blogs with no title receives 400 status code', async () => {
-    const newBlog = {
-        author: 'Connor C',
-        url: 'https://titleless.com',
-        likes: 1000
-    }
+    test('post /api/blogs with no title receives 400 status code', async () => {
+        const newBlog = {
+            author: 'Connor C',
+            url: 'https://titleless.com',
+            likes: 1000
+        }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-        .expect('Content-Type', /application\/json/)
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
 
-})
+    })
 
-test('post /api/blogs with no url receives 400 status code', async () => {
-    const newBlog = {
-        title: 'What is this URL?',
-        author: 'Connor C',
-        likes: 1000
-    }
+    test('post /api/blogs with no url receives 400 status code', async () => {
+        const newBlog = {
+            title: 'What is this URL?',
+            author: 'Connor C',
+            likes: 1000
+        }
 
-    await api
-        .post('/api/blogs')
-        .send(newBlog)
-        .expect(400)
-        .expect('Content-Type', /application\/json/)
+        await api
+            .post('/api/blogs')
+            .send(newBlog)
+            .expect(400)
+            .expect('Content-Type', /application\/json/)
 
-})
+    })
 
-afterAll(async () => {
-  await mongoose.connection.close()
+    afterAll(async () => {
+    await mongoose.connection.close()
+    })
 })
