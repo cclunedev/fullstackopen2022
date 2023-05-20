f7ab6bc68489b84b377d9365ac27bdda55c62cc8
cclune <cclunedev@gmail.com>
2023-05-20 15:25:55 -0400

    Completes Exercise 4.10. Adds POST blogs test and refactors GET and POST to use async/await

--- part4/blog/tests/blog_api.test.js
+++ part4/blog/tests/blog_api.test.js
@@ -14,7 +14,7 @@ beforeEach(async () => {
   await Promise.all(promiseArray)
 })
 
-test('get blogs returns the correct number of blogs', async () => {
+test('get /api/blogs returns the correct number of blogs', async () => {
     const response = await api.get('/api/blogs')
   
     expect(response.body).toHaveLength(helper.initialBlogs.length)
@@ -25,7 +25,30 @@ test('the unique identifier property of the blog posts is named id', async () =>
     expect(response.body[0].id).toBeDefined()
 })
 
+test('post /api.blogs request creates a new blog post', async () => {
+    const newBlog = {
+    
+        title: 'Ripping Dingers',
+        author: 'Connor C',
+        url: 'https://hittingtanks.com',
+        likes: 1000
+    }
 
+    await api
+        .post('/api/blogs')
+        .send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    
+    const response = await api.get('/api/blogs')
+    const contents = response.body.map(r => r.title)
+    expect(contents).toContain(
+        'Ripping Dingers'
+    )
+    expect(response.body).toHaveLength(helper.initialBlogs.length + 1)
+
+})
 
 
 
